a8738ef0864138eb3c1d5661cfff1f3f771125e6
ankur263 <ankur.shrivastava263@gmail.com>
2015-11-09 16:50:02 +0530

    modified the model from skipgram to CBOW

--- w2v_main.lua
+++ w2v_main.lua
@@ -5,24 +5,24 @@ require("torch")
 dofile("w2v_torch.lua")
 
 config = {}
-config.corpus = "text8_newline.txt" 
+config.corpus = "text8_minified_lowercase.txt" 
 config.window = 5 
-config.dim = 100 
+config.dim = 200 
 config.alpha = 0.75
 config.table_size = 1e8
 config.neg_samples = 5 
-config.minfreq = 5 
-config.lr = 0.5 
+config.minfreq = 3 
+config.lr = 0.5
 config.min_lr = 0.4
 config.epochs = 1
 
 m = Word2Vec(config)
 m:build_datastructures(config.corpus)
 --m:build_table()
-
+--os.exit(0)
 for k = 1, config.epochs do
     m.lr = config.lr
-    m:train_data(config.corpus)
+    m:train_data_test(config.corpus)
 end
 
 m:writebinary()
--- w2v_torch.lua
+++ w2v_torch.lua
@@ -24,11 +24,16 @@ function Word2Vec:__init(config)
     self.index2word = {}
     self.word2index = {}
     self.total_count = 0
+    self.numhid2 = 200
+    self.numwords = self.window - 1
 end
+
+dataset = {}
+function dataset:size() return 200 end
 
 function Word2Vec:build_datastructures(corpus)
 
-	print("Creating Datastructures")
+    print("Creating Datastructures")
     local start = sys.clock()
     print("Opening file: "..corpus)
     local f = io.open(corpus, "r")
@@ -36,11 +41,11 @@ function Word2Vec:build_datastructures(corpus)
     print("Creating dictionary")
     for line in f:lines() do
         for _, word in ipairs(self:split(line)) do
-	       self.total_count = self.total_count + 1
-	       if self.vocab[word] == nil then
-	           self.vocab[word] = 1	 
+           self.total_count = self.total_count + 1
+           if self.vocab[word] == nil then
+               self.vocab[word] = 1  
            else
-	           self.vocab[word] = self.vocab[word] + 1
+               self.vocab[word] = self.vocab[word] + 1
            end
         end
         n = n + 1
@@ -51,12 +56,12 @@ function Word2Vec:build_datastructures(corpus)
 
     print("Reducing vocab size")
     for word, count in pairs(self.vocab) do
-    	if count >= self.minfreq then
-    		freq_words=freq_words+1
-     	    self.index2word[#self.index2word+1] = word
-            self.word2index[word] = #self.index2word	    
-    	else
-	       self.vocab[word] = nil
+        if count >= self.minfreq then
+            freq_words=freq_words+1
+            self.index2word[#self.index2word+1] = word
+            self.word2index[word] = #self.index2word        
+        else
+           self.vocab[word] = nil
         end
     end
     print("Frequency reduced")
@@ -64,85 +69,78 @@ function Word2Vec:build_datastructures(corpus)
     
     self.vocab_size = #self.index2word
 
-    self.word_vecs = nn.LookupTable(self.vocab_size, self.dim)
-    self.context_vecs = nn.LookupTable(self.vocab_size, self.dim)
-    self.word_vecs:reset(0.25); self.context_vecs:reset(0.25)
-    self.w2v = nn.Sequential()
-    self.w2v:add(nn.ParallelTable())
-    self.w2v.modules[1]:add(self.context_vecs)
-    self.w2v.modules[1]:add(self.word_vecs)
-    self.w2v:add(nn.MM(false, true))
-    self.w2v:add(nn.Sigmoid())
+    -- self.word_vecs = nn.LookupTable(self.vocab_size, self.dim)
+    -- self.context_vecs = nn.LookupTable(self.vocab_size, self.dim)
+    -- self.word_vecs:reset(0.25); self.context_vecs:reset(0.25)
+    -- self.w2v = nn.Sequential()
+    -- self.w2v:add(nn.ParallelTable())
+    -- self.w2v.modules[1]:add(self.context_vecs)
+    -- self.w2v.modules[1]:add(self.word_vecs)
+    -- self.w2v:add(nn.MM(false, true))
+    -- self.w2v:add(nn.Sigmoid())
     self.decay = (self.min_lr-self.lr)/(self.total_count*self.window)
+    --modified version for CBOW implementation
+    self.model = nn.Sequential();
+    self.word_vecs = nn.LookupTable(self.vocab_size, self.dim);
+    self.model:add( self.word_vecs ); 
+    self.model:add( nn.Reshape(self.numwords*self.dim));  
+    self.model:add( nn.Linear(self.numwords*self.dim, self.numhid2)); 
+    self.model:add( nn.Sigmoid() );                  
+    self.model:add( nn.Linear(self.numhid2, self.vocab_size) );    
+    self.model:add( nn.LogSoftMax() ); 
 
-    local start = sys.clock()
-    local total_count_pow = 0
-    print("creating table ")
-    for _, count in pairs(self.vocab) do
-        total_count_pow = total_count_pow + count^self.alpha
-    end   
-    self.table = torch.IntTensor(self.table_size)
-    local word_index = 1
-    local word_prob = self.vocab[self.index2word[word_index]]^self.alpha / total_count_pow
-    for idx = 1, self.table_size do
-        self.table[idx] = word_index
-        if idx / self.table_size > word_prob then
-            word_index = word_index + 1
-            --print(self.index2word[word_index]);
-            if self.index2word[word_index] ~= nil then
-               word_prob = word_prob + self.vocab[self.index2word[word_index]]^self.alpha / total_count_pow
-            end
-        end
-        if word_index > self.vocab_size then
-            word_index = word_index - 1
-        end
-    end
-    print("table creation done")
+    -- Minimize the negative log-likelihood
+    self.criterion = nn.ClassNLLCriterion();
+    self.trainer = nn.StochasticGradient(self.model, self.criterion);
+    self.trainer.learningRate = self.lr;
+    self.trainer.maxIteration = 2;
+    --maintaining word order
+    self.trainer.shuffleIndices = false;
+
     
+    self.model.vocab = self.word2index;
+    self.model.vocab_ByIndex = self.index2word;
+
 end
 
 
-function Word2Vec:train_context_word(contexts,word)
-    local p = self.w2v:forward({contexts, word})
-    local loss = self.criterion:forward(p, self.labels)
-    local dl_dp = self.criterion:backward(p, self.labels)
-    self.w2v:zeroGradParameters()
-    self.w2v:backward({contexts, word}, dl_dp)
-    self.w2v:updateParameters(self.lr)
-end
+function Word2Vec:train_data_test(corpus)
+    input_batch = torch.IntTensor(1,self.window-1)
+    target_batch = torch.IntTensor(1)
 
-function Word2Vec:sample_contexts(context)
-    self.contexts[1] = context
-    local i = 0
-    while i < self.neg_samples do
-        neg_context = self.table[torch.random(self.table_size)]
-	if context ~= neg_context then
-	    self.contexts[i+2] = neg_context
-	    i = i + 1
-	end
-    end
-end
-
-function Word2Vec:train_data(corpus)
     print("Training Data...")
     local start = sys.clock()
     local c = 0
     f = io.open(corpus, "r")
     print("Iterating over dataset")
+    local batch_count = 1
     for line in f:lines() do
         sentence = self:split(line)
         for i, word in ipairs(sentence) do
-	        word_idx = self.word2index[word]
+            word_idx = self.word2index[word]
             if word_idx ~= nil then -- word exists in vocab
                 local reduced_window = 2
                 self.word[1] = word_idx 
+                local cnt = 1
+                local adjacent_words = torch.IntTensor(self.window - 1)
+        local found = 0
                 for j = i - reduced_window, i + reduced_window do 
+            --print(j .. " j val")
                     local context = sentence[j]
+                    if i == j and context ~= nil then
+            --print(context)
+            --print(self.word2index[context])
+            --print("here1")
+                        target_batch[1] = self.word2index[context]
+            --print("here")
+                    end
                     if context ~= nil and j ~= i then
                         context_idx = self.word2index[context]
+
                         if context_idx ~= nil then
-  		                    self:sample_contexts(context_idx) 
-                            self:train_context_word(self.word,self.contexts) 
+                found = found + 1
+                            adjacent_words[cnt] = context_idx
+                            cnt = cnt + 1
                             c = c + 1
                             self.lr = math.max(self.min_lr, self.lr + self.decay) 
                             if c % 100000 ==0 then
@@ -150,26 +148,49 @@ function Word2Vec:train_data(corpus)
                             end
                         end
                     end
-                end		
+                end
+        if found == 4 then
+            --sending input batch and target batch for training using stochastic gradient
+                input_batch = adjacent_words
+            --print(input_batch)
+            --print(target_batch)
+            --os.exit(0)
+            --print(batch_count)
+                dataset[batch_count] = { input_batch, target_batch }
+            batch_count  = batch_count + 1
+            --print(dataset)
+            found = 0
+        end
+        if batch_count % 201 == 0 then
+            print("Processing Batch... ")
+            --print(dataset)
+            self.trainer:train(dataset);    
+            batch_count = 1
+        end
             end
         end
     end
     print("Training Complete")
 end
 
+
 function Word2Vec:normalize(m)
     m_norm = torch.zeros(m:size())
     for i = 1, m:size(1) do
-    	m_norm[i] = m[i] / torch.norm(m[i])
+        m_norm[i] = m[i] / torch.norm(m[i])
     end
     return m_norm
 end
 
 
 function Word2Vec:writebinary()
-    file = torch.DiskFile('foo.txt', 'w'):ascii()
-
-    wstr=self.vocab_size.." 100\n"
+    print("writing to disk")
+    file = torch.DiskFile('yoonkim_4mb.txt', 'w'):ascii()
+    if file == nil then
+    print("cannot open file")
+    os.exit(0)
+    end
+    wstr=self.vocab_size.." 200\n"
 
     --file:writeInt(self.vocab_size)
     --file:writeString(" 100\n")
@@ -185,17 +206,20 @@ function Word2Vec:writebinary()
         --file:wri
         --print(#w)
         wstr=""
-        for i=1,100 do -- fill up the Storage
-        	wstr=wstr..w[i].." "
+        for i=1,self.dim do -- fill up the Storage
+            wstr=wstr..w[i].." "
+        --print(wstr)
             --file:writeDouble(w[i])
             --file:writeString(" ")
         -- for i in #w do
         --     print(i)
         end
+    --print(wstr)
         file:writeString(wstr.."\n")
         --file:writeObject(w)
 
     end
+    print("closing file")
     file:close() 
 end
 
@@ -209,5 +233,7 @@ function Word2Vec:split(input, sep)
     end
     return t
 end
+
+
 
 
